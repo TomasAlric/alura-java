19cb12a30831eddc13f64e6f94813ce3070b581b
Tomas Alric <tomas.alricc@gmail.com>
2022-09-14 10:17:34 -0300

    Polimorfismo, referências genéricas

--- bytebank-herdado/src/TesteReferencias.java
+++ bytebank-herdado/src/TesteReferencias.java
@@ -1,11 +1,21 @@
 public class TesteReferencias {
     public static void main(String[] args) {
 
-        Funcionario g1 = new Gerente();
-
+        Gerente g1 = new Gerente();
         g1.setNome("Marcos");
-        System.out.println(g1.getNome());
+        g1.setSalario(5000.0);
 
+        Funcionario f1 = new Funcionario();
+        f1.setSalario(2000.0);
 
+        EditorVideo e1 = new EditorVideo();
+        e1.setSalario(2500.0);
+
+        ControleBonficacao controle = new ControleBonficacao();
+        controle.registra(g1);
+        controle.registra(f1);
+        controle.registra(e1);
+
+        System.out.println(controle.getSoma());
     }
 }
